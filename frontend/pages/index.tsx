ec3812ca94ae6f635b60ae3f87a940ec966fc895
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-12-27 20:11:09 +0900

    카카오 브라우저 확인

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,27 +1,48 @@
 import * as S from "styles/home/style";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import intro from "public/img/intro.png";
 export default function Intro() {
   const router = useRouter();
   const { data: session, status } = useSession<any>({ required: false });
+  const [isKakaoBrower, setKakaoBrower] = useState(false);
   const onClickEnter = () => {
-    if (status === "authenticated") {
-      console.log(session);
-      router.push("signup");
+    if (isKakaoBrower) {
+      alert(
+        "카카오 인앱 브라우저에서는 로그인이 지원되지 않습니다. 다른 브라우저로 접속해주세요"
+      );
     } else {
-      signIn("google", { callbackUrl: "/signup" });
+      if (status === "authenticated") {
+        console.log(session);
+        router.push("signup");
+      } else {
+        signIn("google", { callbackUrl: "/signup" });
+      }
     }
   };
   const onClickCreate = () => {
-    if (status === "authenticated") {
-      console.log(session);
-      router.push("signup");
+    if (isKakaoBrower) {
+      alert(
+        "카카오 인앱 브라우저에서는 회원가입이 지원되지 않습니다. 다른 브라우저로 접속해주세요"
+      );
     } else {
-      signIn("google", { callbackUrl: "/signup" });
+      if (status === "authenticated") {
+        console.log(session);
+        router.push("signup");
+      } else {
+        signIn("google", { callbackUrl: "/signup" });
+      }
     }
   };
+
+  useEffect(() => {
+    const isKakao = navigator.userAgent.match("KAKAOTALK");
+    console.log(navigator.userAgent);
+    // alert(isKakao);
+    setKakaoBrower(Boolean(isKakao));
+  }, []);
   // console.log(process.env.NEXT_PUBLIC_BASE_URL);
   return (
     <S.Container>
@@ -39,9 +60,10 @@ export default function Intro() {
         {status !== "authenticated" && (
           <S.Btn onClick={onClickCreate}>내 앨범 만들기</S.Btn>
         )}
-
-        {/* <button onClick={() => signOut()}>Sign Out</button> */}
-        {session && <div>{session?.user?.email}</div>}
+        {/* {session && <div>{session?.user?.email}</div>} */}
+        {/* {isKakaoBrower && <div>카카오임</div>}
+        {!isKakaoBrower && <div>카카오 아님</div>} */}
+        {/* {session && <div>{session?.user?.email}</div>} */}
       </S.ContentContainer>
     </S.Container>
   );
